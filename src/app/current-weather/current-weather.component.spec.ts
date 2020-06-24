6acd8a9278e474ac885ea2218e8e78f13f79466f
Daniel Gross <danwgross@gmail.com>
2020-06-24 10:24:51 -0400

    unit tests passing

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,27 +1,68 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 import { WeatherService } from '../weather/weather.service';
-import { WeatherServiceFake } from '../weather/weather.service.fake';
+import { fakeWeather } from '../weather/weather.service.fake';
 import { CurrentWeatherComponent } from './current-weather.component';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
   let fixture: ComponentFixture<CurrentWeatherComponent>;
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>;
 
   beforeEach(async(() => {
+    const weatherServiceSpy = jasmine.createSpyObj('WeatherService', [
+      'getCurrentWeather',
+    ]);
     TestBed.configureTestingModule({
       declarations: [CurrentWeatherComponent],
-      providers: [{ provide: WeatherService, useClass: WeatherServiceFake }],
+      providers: [{ provide: WeatherService, useValue: weatherServiceSpy }],
     }).compileComponents();
+    weatherServiceMock = TestBed.inject(WeatherService) as any;
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
     expect(component).toBeTruthy();
+  });
+
+  it('should get currentWeather from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1);
+  });
+  it('should load currentWeather in Fake City from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather));
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    expect(component.current).toBeDefined();
+    expect(component.current.city).toEqual('Fake City');
+    expect(component.current.temperature).toEqual(72);
+
+    // Assert on DOM
+    const debugElement = fixture.debugElement;
+    const titleElement: HTMLElement = debugElement.query(By.css('span')).nativeElement;
+    expect(titleElement.textContent).toContain('Fake City');
   });
 });
